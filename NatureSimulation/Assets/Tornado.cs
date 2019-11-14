c0b85bdfac20a7b1ef7f9816db86aa8412c9329f
Cattatonicat <threeeyedvenelope@gmail.com>
2019-11-13 23:26:10 -0600

    Set up tornado

---
+++ NatureSimulation/Assets/Tornado.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tornado : MonoBehaviour
+{
+    [SerializeField]
+    private float tornadoSpeed = 0;
+
+    private Transform tornadoTransform = null;
+
+    private void Awake()
+    {
+        tornadoTransform = GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        MoveTornado();
+    }
+
+    private void MoveTornado()
+    {
+        float tornadoPositionX = tornadoTransform.position.x;
+        float tornadoPositionY = tornadoTransform.position.y;
+        float tornadoPositionZ = tornadoTransform.position.z;
+
+        tornadoPositionX += tornadoSpeed;
+        tornadoPositionZ += tornadoSpeed;
+        tornadoPositionX -= tornadoSpeed;
+        tornadoPositionZ -= tornadoSpeed;
+
+
+        tornadoTransform.position = new Vector3(tornadoPositionX, tornadoPositionY, tornadoPositionZ);
+    }
+
+
+}
